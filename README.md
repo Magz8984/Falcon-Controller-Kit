a9b9d6cff08d7b0323f3645f18b399d30404949a
Magz8984 <collinsmuthinja@gmail.com>
2019-02-01 10:46:24 +0300

    Initialize ReadMe

---
+++ README.md
@@ -0,0 +1,1 @@
+# Falcon Controller Kit
--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "EFS.h"
-
-
-//Test code for header files
-int main(int arg,char *agrs[],char *env[]){
-   return 0;
-}
