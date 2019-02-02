e5a27bf2fbd925e8fbca4a0b70a009ed512d2644
Magz8984 <collinsmuthinja@gmail.com>
2019-02-02 18:57:47 +0300

    Leave Angular  Constats

---
+++ EFS.h
@@ -0,0 +1,15 @@
+// This is  script converts angles into motions based angle and strength
+// Angle determines yaw ,roll and pitch. Strength determines how much roll or pitch
+#include <math.h>
+
+#define MAX_RPMS 2000  // Dependent on esc calib values
+#define MIN_RPMS 1050
+
+#define FRONT_MIN 45
+#define FRONT_MAX 135
+#define BACK_MIN 225
+#define BACK_MAX 315
+#define LEFT_MIN 136
+#define LEFT_MAX 224
+#define RIGHT_MIN 315
+#define RIGHT_MAX 44
