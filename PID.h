2b53f0c6c2ac411acb8591809eb7916ee42ee1f1
Magz8984 <collinsmuthinja@gmail.com>
2019-02-02 20:10:25 +0300

    Create Error Time

--- PID.h
+++ PID.h
@@ -1,15 +1,38 @@
 //Header file denotes PID Controller
 
+void init_command_variable(int command_variable);
+void errorTerm(int  control_variable);
+void init();
+void freeMem();
 
 typedef struct Plant{
   int ac_signal;
   int controlled_variable;
-}
+  int error_term;
+}Plant;
 
+Plant *plant;
 enum speeds{HIGH,LOW};
-
 int commandVariable=0;
 
-int errorTerm(int  control_variable){
-  return commandVariable - control_variable;
+
+void init() {
+//init code
+plant=(Plant*) malloc(sizeof(Plant));
 }
+
+void freeMem(){
+ free(plant);
+}
+
+void errorTerm(int  control_variable){
+  plant->controlled_variable=control_variable;
+  plant->error_term=(commandVariable-control_variable);
+}
+
+void init_command_variable(int command_variable){
+  commandVariable=command_variable;
+}
+
+
+//Controller and Rates
